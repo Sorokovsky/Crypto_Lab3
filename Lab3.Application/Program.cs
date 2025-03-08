7fda5ce5386d4ecd23020cd7e8a14fbc3637d2b4
Sorokovsky <Sorokovskys@ukr.net>
2025-03-08 19:18:28 +0200

    feat: start refactor

---
+++ Lab3.Application/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab3.Core;
+
+namespace Lab3.Application;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var generator = new PrimeNumberGenerator();
+        var key = generator.Generate();
+        var encryption = new SymmetricEncryption();
+        var s = encryption.EncryptFile("files/in.txt", "files/out.txt", key.ToString());
+        Console.WriteLine("---> {0}", s);
+        encryption.DecryptFile("files/out.txt", "files/out2.txt", s);
+    }
+}
---
+++ Lab3.Core/SymmetricEncryptionByte.cs
@@ -0,0 +1,165 @@
+﻿namespace Lab3.Core;
+
+public class SymmetricEncryptionByte
+{
+    private const int SizeOfBlock = 16;
+    private static int AddOfByte = 0;
+    private const int ShiftKey = 2;
+    private const int QuantityOfRounds = 16;
+    private byte[][] Blocks;
+
+    public byte[] UlongTyByte(long k)
+    {
+        var key = new byte[4];
+        for (var i = 0; i < 4; i++)
+        {
+            key[3 - i] = (byte)((k & (255 << i * 8)) >> i * 8);
+        }
+        return key;
+    }
+
+    private byte[] ByteToRightLength(byte[] input)
+    {
+        AddOfByte = 0;
+        if ((input.Length % SizeOfBlock) != 0)
+        {
+            AddOfByte = SizeOfBlock - input.Length % SizeOfBlock;
+            Array.Resize(ref input, input.Length + AddOfByte);
+        }
+
+        return input;
+    }
+
+    private byte[] ByteAllToBit(byte[] input)
+    {
+        var bit = new byte[input.Length * 8];
+
+        for (var k = 0; k < input.Length; k++) ByteOneToBit(input[k], k);
+        return bit;
+
+        void ByteOneToBit(byte one, int k)
+        {
+            for (var i = 0; i < sizeof(byte) * 8; i++)
+            {
+                var index = k * 8 - i + 7;
+                if ((one & (1 << i)) != 0)
+                {
+                    bit[index] = 1;
+                }
+                else
+                {
+                    bit[index] = 0;
+                }
+            }
+        }
+    }
+
+    private byte[] BitAllToByte(byte[] input)
+    {
+        var bytes = new byte[input.Length / (sizeof(byte) * 8)];
+        var temp = new byte[sizeof(byte) * 8];
+        for (var k = 0; k < input.Length / 8; k++)
+        {
+            Array.Copy(input, k * 8, temp, 0, 8);
+            BitsToOneByte(temp, k);
+        }
+
+        return bytes;
+
+        void BitsToOneByte(byte[] one, int k)
+        {
+            byte n = 1, b = 0;
+            for (var i = sizeof(byte) * 8 - 1; i >= 0; i--, n *= 2) b += (byte)(one[i] * n);
+            bytes[k] = b;
+        }
+    }
+
+    private void CutByteIntoBlocks(byte[] input)
+    {
+        Blocks = new byte[input.Length / SizeOfBlock][];
+        var numberOfBlock = input.Length / SizeOfBlock;
+        for (var i = 0; i < numberOfBlock; i++) Blocks[i] = new byte[SizeOfBlock];
+        for(var i = 0; i < numberOfBlock; i++)
+        {
+            Array.Copy(input, i * SizeOfBlock, Blocks[i], 0, SizeOfBlock);
+        }
+    }
+
+    private byte[] CorrectKeyWord(byte[] input, int keyLength)
+    {
+        Array.Resize(ref input, keyLength);
+        return input;
+    }
+
+    private byte[] XOR(byte[] first, byte[] second)
+    {
+        var result = new byte[first.Length];
+        for (var i = 0; i < first.Length; i++)
+        {
+            result[i] = (byte)(((int)first[i] ^ (int)second[i]));
+        }
+
+        return result;
+    }
+    
+    private byte[] F(byte[] first, byte[] second) => XOR(first, second);
+
+    private byte[] EncodeDesOneRound(byte[] input, byte[] key)
+    {
+        var k = input.Length / 2;
+        var temp = new byte[k];
+        var result = new byte[input.Length];
+        var l = new byte[k];
+        var r = new byte[k];
+        Array.Copy(input, 0, l, 0, k);
+        Array.Copy(input, k, r, 0, k);
+        temp = XOR(l, F(r, key));
+        Array.Copy(r, 0, result, 0, k);
+        Array.Copy(temp, 0, result, k, k);
+        return result;
+    }
+
+    private byte[] DecodeDesOneRound(byte[] input, byte[] key)
+    {
+        var k = input.Length / 2;
+        var temp = new byte[k];
+        var result = new byte[input.Length];
+        var l = new byte[k];
+        var r = new byte[k];
+        Array.Copy(input, 0, l, 0, k);
+        Array.Copy(input, k, r, 0, k);
+        temp = XOR(F(l, key), r);
+        Array.Copy(temp, 0, result, 0, k);
+        Array.Copy(l, 0, result, k, k);
+        return result;
+    }
+
+    private byte[] KeyToNextRound(byte[] key)
+    {
+        var bits = ByteAllToBit(key);
+        byte first;
+        for (var i = 0; i < ShiftKey; i++)
+        {
+            first = bits[bits.Length - 1];
+            for (var j = bits.Length - 1; j >= 0; j--) bits[j] = bits[j - 1];
+            bits[0] = first;
+        }
+
+        key = BitAllToByte(bits);
+        return key;
+    }
+
+    private byte[] KeyToPreviousRound(byte[] key)
+    {
+        var bits = ByteAllToBit(key);
+        byte first;
+        for (var i = 0; i < ShiftKey; i++)
+        {
+            first = bits[0];
+            for(var j = 0; j < bits.Length - 1; j++) bits[j] = bits[j + 1];
+            bits[bits.Length - 1] = first;
+        }
+        key = BitAllToByte(bits);
+        return key;
+    }
+}
