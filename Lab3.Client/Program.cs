d67e005282567b721de32fae958b165882266521
Sorokovskys <Sorokovskys@ukr.net>
2025-03-17 15:48:40 +0200

    bug

--- Lab3.Client/Program.cs
+++ Lab3.Client/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Lab3.Client;
+﻿using Lab3.Client;
 
 var clientCreated = SocketClient.TryCreate("127.0.0.1", 8080, out var client);
 if (clientCreated)
