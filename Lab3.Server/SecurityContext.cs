f71a5217b24457c1a6aeedff69516d2bb4d9748f
Sorokovsky <Sorokovskys@ukr.net>
2025-04-12 23:41:54 +0300

    server security start

---
+++ Lab3.Server/SecurityContext.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab3.Server;
+
+public class SecurityContext
+{
+    private static SecurityContext? _instance;
+    private readonly UserTable _userTable = new();
+
+    public static SecurityContext Instance => GetInstance();
+
+    public bool Contains(string value)
+    {
+        return _userTable.Contains(value);
+    }
+
+    public void Register(string value, string decodeKey)
+    {
+        _userTable.Register(value, decodeKey);
+    }
+
+    private static SecurityContext GetInstance()
+    {
+        if (_instance is null) _instance = new SecurityContext();
+        return _instance;
+    }
+}
---
+++ Lab3.Server/UserTable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lab3.Server;
+
+public class UserTable
+{
+    private readonly IDictionary<string, string> _users = new ConcurrentDictionary<string, string>();
+
+    public bool Contains(string value)
+    {
+        return _users.ContainsKey(value);
+    }
+
+    public void Register(string value, string decodeKey)
+    {
+        _users.TryAdd(value, decodeKey);
+    }
+}
