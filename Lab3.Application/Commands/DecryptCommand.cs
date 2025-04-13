9107a9f7fa2cda1271fbf474e1284ca00fb66b22
Sorokovsky <Sorokovskys@ukr.net>
2025-04-13 18:06:41 +0300

    refactor

--- Lab3.Application/Commands/DecryptCommand.cs
+++ Lab3.Application/Commands/DecryptCommand.cs
@@ -15,25 +15,28 @@ public class DecryptCommand : BaseCommand
     {
         var service = new FilesService();
         var file = service.GetPath(Choosing.GetText("файл"));
-        if (service.Exists(file, false))
+        if (!service.Exists(file, false))
         {
-            var bytes = ByteFilesService.ReadBytes(file);
-            var encryption = new SymmetricEncryption();
-            var key = Choosing.GetNumber("ключ", null, null);
-            var output = service.GetPath(Choosing.GetText("вихідний файл"));
-            try
-            {
-                var result = encryption.Decrypt(bytes, BitConverter.GetBytes(key));
-                var text = Encoding.UTF8.GetString(result);
-                service.Write(output, text, false);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
-            Console.WriteLine($"Розшифрований текст: {service.Read(output, false)}");
+            Console.WriteLine("Файл не знайдено.");
+            return;
         }
+
+        var bytes = ByteFilesService.ReadBytes(file);
+        var encryption = new SymmetricEncryption();
+        var key = Choosing.GetNumber("ключ", null, null);
+        var output = service.GetPath(Choosing.GetText("вихідний файл"));
+        try
+        {
+            var result = encryption.Decrypt(bytes, BitConverter.GetBytes(key));
+            var text = Encoding.UTF8.GetString(result);
+            service.Write(output, text, false);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        Console.WriteLine($"Розшифрований текст: {service.Read(output, false)}");
     }
 }
